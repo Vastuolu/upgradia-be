36b42cf56be495368cc7bcd0d4d146179044f220
Kontra_N356 <naufalnabillansyah49@gmail.com>
2023-09-23 21:35:52 +0700

    fix: change paragraph data type and no data handler

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { userRoutes } from './src/app/user/routes'
 import { projectRoutes } from './src/app/project/routes.project';
+import { blogRoutes } from './src/app/blog/routes.blog';
 import path from 'path'
 
 dotenv.config();
@@ -14,6 +15,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/user', userRoutes)
 app.use('/project',projectRoutes)
+app.use('/blog', blogRoutes)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
