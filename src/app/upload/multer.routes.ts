9d6728ad64179515b645f73ab77208dd6bf46189
RFP11 <lolscam104@gmail.com>
2023-09-28 18:31:56 +0700

    bug:upload files

--- src/app/upload/multer.routes.ts
+++ src/app/upload/multer.routes.ts
@@ -1,19 +1,35 @@
 import { Router, Request, Response, response } from 'express'
 import { upload, saveFileBlog, saveFileProject} from './multer'
 import { respond, returnHandler as retHandler } from '../../helper/response'  
+import { type } from 'os'
 
 
 export const FileRoutes = Router()
 
 FileRoutes.post('/project', upload.array('image' , 3), async (req: Request, res: Response)=>{
-    	if(!req.file){return retHandler(500, true, "There Is No Files", null)}
-        console.log(req.file)
-        return respond(200, false, "bla bla", null, res)
-	// const fileName = req.file.originalname       
-    // const id:number = parseInt(req.body.projectId)
-	// const {data,isError,message,status} = await saveFileProject(fileName, id)
-    // return respond(status,isError, message, data, res)
-}
+    	console.log('here') 
+        const files = req.files
+        
+        if(!files) return respond(304, true, "No image found", null, res)
+
+        console.log(files?.length)
+        // console.log(files.forEach)
+        console.log(req.files)
+
+        // const filename = files.
+        const projectId = req.body.projectId
+        const projectIdAsNumber = parseInt(projectId, 10);
+        const filenames:string[] = []
+        files.forEach(async(file:File[])=>{
+            const value = file['originalname']
+            // const data = await saveFileProject(value, projectId)
+            // console.log(data)
+            filenames.push(value)
+        })
+        return respond(200, false, "Ok", filenames, res)
+        
+    
+    }      
 )
 
 FileRoutes.post('/blog', upload.array('image', 3), async (req: Request, res: Response)=>{
